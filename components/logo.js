5e693af61c2086a3e85c47cd54b195485a11af35
Belo <barazyuuzi2000@gmail.com>
2023-03-12 07:16:21 +0900

    alter logo + pic + name

--- components/logo.js
+++ components/logo.js
@@ -6,12 +6,17 @@ export default function Logo() {
     <div className="basic max-w-7xl mx-auto px-8 py-6">
       <Image
        src="/yamada.png"
-       width={70}
-       height={45}
+       width={130}
+       height={130}
       />
-    <Link href="/" className='box'>
+      <Image
+       src="/test.png"
+       width={1000}
+       height={450}
+      />
+    {/* <Link href="/" className='box'>
       <h1 className='text-6xl font-fancy1'>Belocump</h1>
-    </Link>
+    </Link> */}
      </div>
   )
 }
