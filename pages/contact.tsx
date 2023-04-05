7552fdcead44b2676183a2a95ecd28d8111ef139
Belo <barazyuuzi2000@gmail.com>
2023-04-05 10:38:09 +0900

    コンタクトのスタイルを当てる

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,7 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useRef} from "react";
+import React, { useRef } from "react";
 
 export default function Home() {
-
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const messageRef = useRef<HTMLTextAreaElement>(null);
@@ -31,55 +30,73 @@ export default function Home() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-
-        <div className="container mt-5 ">
-        <h2 className="mb-3">Contact to Belocump</h2>
+    <div className="container m-5 h-full w-full mx-auto md:w-1/2">
+      <div className="bg-yellow-50">
+        <h2 className="text-3xl font-fancy1 mb-10 text-center">
+          Contact to Belocump
+        </h2>
         <form
           onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}
         >
-          <div className="mb-3">
-            <label className="form-label" htmlFor="name">
+          <div className="w-full">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="name"
+            >
               お名前
             </label>
             <input
-              className="form-control"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               type="text"
               id="name"
-              placeholder='（例)_山田　太郎'
+              placeholder="（例)　山田　太郎"
               required
               ref={nameRef}
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="email">
+
+          <div className="w-full">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="email"
+            >
               メールアドレス
             </label>
             <input
-              className="form-control"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               type="email"
               id="email"
-              placeholder='（例)_aaa@aa.com'
+              placeholder="（例)　aaa@aa.com"
               required
               ref={emailRef}
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="message">
+          <div className="w-full">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="message"
+            >
               メッセージ
             </label>
             <textarea
-              className="form-control"
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               id="message"
+              placeholder="メッセージをここに書いてください"
               required
               ref={messageRef}
             />
           </div>
-          <button className="btn btn-danger" type="submit">
-            メール送信
-          </button>
+          <hr />
+          <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              メール送信
+            </button>
+          </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
